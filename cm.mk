680b59035164edf9b8027a4524d83a996a8d05bd
Janne Grunau <j@jannau.net>
2012-04-15 13:24:26 +0200

    add missing PRODUCT_ to PRODUCT_CHARACTERISTICS

--- cm.mk
+++ cm.mk
@@ -16,7 +16,7 @@ PRODUCT_DEVICE := startablet
 PRODUCT_MODEL := LG-V909
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_startablet BUILD_ID=HMJ37 BUILD_FINGERPRINT=lge/lge_startablet/v909:3.1/HMJ37/v10o.41FD770E:user/release-keys PRIVATE_BUILD_DESC="startablet-user 3.1 HMJ37 release-keys"
-CHARACTERISTICS=tablet,nosdcard
+PRODUCT_CHARACTERISTICS=tablet,nosdcard
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := startablet
