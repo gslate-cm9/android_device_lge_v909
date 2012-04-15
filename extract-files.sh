a54e50d15594c9836c3fc6c740149c3d742531be
Janne Grunau <j@jannau.net>
2012-04-15 19:00:52 +0200

    unify proprietary-files*, extract-files.sh works only with an existing CM9 image

--- extract-files.sh
+++ extract-files.sh
@@ -14,17 +14,4 @@ for FILE in `cat proprietary-files.txt`; do
     ${CMD} "${SOURCE}/$FILE" $BASE/$FILE
 done
 
-VENTANA_ICS_MOUNT=${VENTANA_SYSTEM_MOUNT:-/mnt/tmp/}
-
-if [ ! -d "$VENTANA_ICS_MOUNT/lib/egl" ] ; then
-    echo "nvidia ICS binaries not found"
-    exit 1
-fi
-
-while read FILE; do
-    DIR=$(dirname ${FILE})
-    test -d "${BASE}/${DIR})" || mkdir -p "${BASE}/${DIR}"
-    cp "${VENTANA_ICS_MOUNT}/${FILE}" "${BASE}/${FILE}"
-done < proprietary-files-ventana.txt
-
 ./setup-makefiles.sh
